2fdbc8f58dfdd2f65bf7a863f8b9904fd3ef3f33
Julio César <julioc255io@gmail.com>
2014-10-26 14:44:13 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Stream Sensor Server
+
+
+Este modulo implementado en Nodejs recive los datos de la app en android y los emite a los clientes, estos seran presentados a los usuarios finales en la plataforma que sera desarollada en Django.
+
+
+
+### Uso:
+
+En Linux debes tener instalado Nodejs y NPM
+
+Para instalarnos
+```
+ $ sudo apt-get install node
+```
+Entra al directorio del proyecto y ejecutalo
+
+```
+$ node server.js
+```
---
+++ server.js
@@ -0,0 +1,67 @@
+/**
+Socket recepor del streaming enviado desde la app de android,
+* encargado de recibir los datos binarios (Buffers) y codificarlos
+* para luego mostrarlos graficados en el navegador con Javascript.
+*
+* [TODO]
+*    - Investigar formas de tratamiento de datos binarios.
+*    - Indagar sobre la escalabilidad de Sokects.
+*
+* Julio César
+* @uzi200
+* 2014
+**/
+
+var io      =  require('socket.io').listen(3131),
+    colors  =  require("colors"),
+    fs      =  require('fs'),
+    ip      =  require('ip'),
+    net     =  require('net');
+
+var  out;
+
+
+server = net.createServer(function (socket) {
+    socket.setEncoding("utf-8");
+    socket.on('data', function (d) {
+
+      //var writable = fs.createWriteStream('file2.txt');
+      //writable.write(d, 'utf-8');
+      // Se recive como string desde el metodo Output Stream
+
+    console.log("CLIENTE : ",d.red);
+    // Luego se hace una conversión a entero para presentarlo al cliente
+    out = parseFloat(d);
+    console.log("SERVER  : ".green, out);         
+      //require('util').log(temp.bold.green);
+    });
+
+
+
+}).listen(8000);
+
+console.log("#######################")
+console.log("Esperando en el puerto:", server.address().port)
+console.log("IP Local:", ip.address())
+
+
+  
+io.sockets.on('connection', function (socket) {
+
+ var max = 100
+
+  // Genera una muestra cada segundo
+
+    setInterval(function() {
+        var x = (new Date()).getTime(), // tiempo actual
+            y = out
+        socket.emit('sample', {
+          x: x,
+          y: y
+        });
+        console.info("emitted: [" + x + "," + y + "]");
+    }, 1000);
+
+
+
+});
